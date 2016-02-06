601c80693b276c4cc22107f85fd927a28b7287ad
Andrew Trigg <andrewt51@hotmail.com>
2016-02-06 00:04:34 +0000

    List page functional with links, detail page not persisting

--- main.js
+++ main.js
@@ -12,28 +12,45 @@ let myApp = angular.module("myApp",["ui.router"])
     })
 
     .state('singlePost',{
-      url: "post/:id",
-      templateUrl: "./templates/singlePost.html"
+      url: "/post/:id",
+      templateUrl: "./templates/postContent.html",
+      controller: "singlePost",
+      params: {item:null}
     })
   })
 
-
-
-
-.controller("mainCtrl", function ($scope,userService){
-  $scope.flickrUserPage = "https://www.flickr.com/photos/";
+.controller("mainCtrl", function ($scope,userService,$state,store){
+  $scope.itemDetail = function(item){
+    store.setFeed(item)
+    $state.go("singlePost",{item: item})
+  }
   let myData = userService.getData()
   .then(function success(data){
-    $scope.feeds = data.data.items;
-    
+
+    // Receive the data and immediately adjust some of it with regexp to get the relevant test to display
+    var temp = data.data.items; 
+    $scope.feeds = temp.map(function(feed){
+      feed.author = feed.author.match(/\((.+)\)/)[1];
+      feed.description = feed.description.replace(/(<.+?>)/g, "");
+      feed.usersPage = "https://www.flickr.com/photos/" + feed.author_id;
+    return feed
+    })
+
   }, function error(err){
     console.log("Error: ",err)
   })
 })
 
+.controller("singlePost", function($scope,userService,store){
+  $scope.item = store.getfeed;
+})
+
 .service('userService', function ($http) {
   this.getData = function (data) {
     return $http.jsonp("https://api.flickr.com/services/feeds/photos_public.gne?tags=potato&tagmode=all&format=json&jsoncallback=JSON_CALLBACK")
+  }
+  this.storeState = function(){
+
   }
 })
 
@@ -42,4 +59,31 @@ let myApp = angular.module("myApp",["ui.router"])
     templateUrl: "directives/authorPanel.html"
   }
 })
+
+.factory('store', function(){
+  return {
+    getFeed : '',
+    setFeed: function(feed) {
+
+      this.getfeed = feed
+    }
+  }
+})
+
+.filter('truncate', function () {
+  return function (text, length, end) {
+    if (isNaN(length)){
+      length = 10; 
+    }
+    if (end === undefined){
+      end = "...";
+    }
+    if (text.length <= length || text.length - end.length <= length) {
+      return text;
+    }
+    else {
+      return String(text).substring(0, length-end.length) + end;
+    }
+  };
+});
 
